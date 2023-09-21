a6b7a99aa41e2968aad12452cf275a7d303ff4f0
Chih Ying <spacetime0311@gmail.com>
2023-09-21 14:33:48 +0800

    feat: FAILED code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Basic arithmetic:");
     println!("add(1, 2): {}", my_test::add(1, 2));
-    println!("sub(1, 2): {}", my_test::sub(1, 2));
+    println!("sub(1, 2): {}", my_test::sub(1, 2))
     println!("mul(1, 2): {}", my_test::mul(1, 2));
     println!("div(1, 2): {}", my_test::div(1, 2));
 }
